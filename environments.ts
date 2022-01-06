661a361d450118d07adbf88e711539433f34e058
Note45 <antonion.2048@gmail.com>
2022-01-06 16:14:33 -0300

    add lib mongoose

--- environments.ts
+++ environments.ts
@@ -6,3 +6,7 @@ dotenv.config();
 const envs = process.env;
 
 export const ENVS = getEnvs(envs as any);
+
+export const MONGO_STRING_URI = `mongodb://${
+  ENVS.DB_USER && ENVS.DB_PASS ? `${ENVS.DB_USER}:${ENVS.DB_PASS}@` : ''
+}${ENVS.DB_HOST}:${ENVS.DB_PORT}/${ENVS.DB_NAME}`;
