7a3b9f434a472dee5df54bd0d88163a2d1b70a99
Note45 <antonion.2048@gmail.com>
2022-01-06 16:13:00 -0300

    add base class to control and check id

---
+++ src/shared/utils/unique-entity-id.utils/identifier.ts
@@ -0,0 +1,30 @@
+import { Types } from 'mongoose';
+
+export class Identifier<T> {
+  constructor(private value: T) {
+    this.value = value;
+  }
+
+  equals(id?: Identifier<T>): boolean {
+    if (id === null || id === undefined) return false;
+    if (!(id instanceof this.constructor)) return false;
+
+    return id.toValue() === this.value;
+  }
+
+  toString() {
+    return String(this.value);
+  }
+
+  /**
+   * Return raw value of identifier
+   */
+
+  toValue(): T {
+    return this.value;
+  }
+
+  toObjectID(): Types.ObjectId {
+    return new Types.ObjectId(this.toString());
+  }
+}
---
+++ src/shared/utils/unique-entity-id.utils/index.ts
@@ -0,0 +1,30 @@
+import * as mongoose from 'mongoose';
+import { Identifier } from './identifier';
+
+// eslint-disable-next-line no-use-before-define
+export type IdTypes = string | UniqueEntityID | mongoose.Types.ObjectId;
+
+const isObjectId = (id: any) => id instanceof mongoose.Types.ObjectId;
+// eslint-disable-next-line no-use-before-define
+const isUniqueId = (id: any) => id instanceof UniqueEntityID;
+
+export class UniqueEntityID extends Identifier<string> {
+  constructor(id: IdTypes = String(new mongoose.Types.ObjectId())) {
+    if (isObjectId(id)) {
+      super(String(id));
+    } else if (isUniqueId(id)) {
+      super(id.toString());
+    } else if (typeof id === 'string') {
+      super(id);
+    }
+  }
+
+  public static validate(id: string) {
+    if (mongoose.Types.ObjectId.isValid(id)) return true;
+    return false;
+  }
+
+  toObjectID(): mongoose.Types.ObjectId {
+    return new mongoose.Types.ObjectId(this.toString());
+  }
+}
