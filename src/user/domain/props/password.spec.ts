0107442fd3d8852c5b38d686ff84a84561f4a1ec
Note45 <antonion.2048@gmail.com>
2022-01-10 09:49:27 -0300

    add password class with tests

---
+++ src/user/domain/props/password.spec.ts
@@ -0,0 +1,55 @@
+import { DomainError } from '../../../shared/core/domain/domain-errors';
+import { IPassword } from '../interfaces/password.interface';
+import { Password } from './password';
+
+const describeBaseTest = 'test password domain field';
+const describeFailedTest = 'test domain errors password field';
+
+describe(describeBaseTest, () => {
+  const passwordNotHashed: IPassword = {
+    value: '$2a$10$YzYPT6tbma80.hCx5yJb6u2YaygoZ65fQVuVIySqC3KtyFXBCHZQu',
+    hashed: false,
+  };
+
+  it('should be able to create a hashed password', () => {
+    const PasswordHashed = Password.create(passwordNotHashed) as Password;
+
+    const PasswordIsHashed = PasswordHashed.isAlreadyHashed();
+
+    expect(PasswordIsHashed).toEqual(true);
+    expect(PasswordHashed.value).toEqual(passwordNotHashed.value);
+  });
+
+  it('should creat hashed password with same hashed passed', () => {
+    const PasswordHashedProps = { ...passwordNotHashed, hashed: true };
+
+    const PasswordHashed = Password.create(PasswordHashedProps) as Password;
+
+    expect(PasswordHashed.props.value).toEqual(PasswordHashedProps.value);
+  });
+
+  it('should create a password hashed equal to user password', async () => {
+    const PasswordPlainText = 'nova-Password-plain-text';
+    const PasswordHashed = Password.create({
+      value: PasswordPlainText,
+    }) as Password;
+
+    const PasswordIsEqualHashed = await PasswordHashed.comparePassword(
+      PasswordPlainText,
+    );
+
+    expect(PasswordIsEqualHashed).toEqual(true);
+  });
+});
+
+describe(describeFailedTest, () => {
+  it('should test error return password field has no minimum length!', () => {
+    const PasswordHashed = Password.create({
+      value: '1234',
+    });
+
+    expect(PasswordHashed as DomainError).toMatchObject({
+      message: 'The password field has no minimum length!',
+    });
+  });
+});
---
+++ src/user/domain/props/password.ts
@@ -0,0 +1,77 @@
+/* eslint-disable no-use-before-define */
+import * as bcrypt from 'bcryptjs';
+import { ValueObject } from '../../../shared/core/domain/value-object';
+import { Guard } from '../../../shared/utils/guard.utils';
+import { IUserError, UserError } from '../user.domain.errors';
+import { IPassword } from '../interfaces/password.interface';
+
+export type PasswordType = IUserError | Password;
+
+export type PasswordProps = Omit<IPassword, 'value'> & {
+  value: string | IPassword;
+};
+
+const BcryptRegex = /^\$2[aby]?\$\d{1,2}\$[./A-Za-z0-9]{53}$/;
+
+export class Password extends ValueObject<IPassword> {
+  get value() {
+    return this.props.value;
+  }
+
+  get hashed() {
+    return this.isAlreadyHashed();
+  }
+
+  // eslint-disable-next-line no-useless-constructor
+  private constructor(props: IPassword) {
+    super(props);
+  }
+
+  private static hashPassword(password: string) {
+    return bcrypt.hashSync(password, 10);
+  }
+
+  public async comparePassword(plainTextPassword: string): Promise<boolean> {
+    const hashed = this.props.value;
+    return bcrypt.compareSync(plainTextPassword, hashed);
+  }
+
+  public isAlreadyHashed(): boolean {
+    return BcryptRegex.test(this.value);
+  }
+
+  public static isBcryptHash(value: string): boolean {
+    return BcryptRegex.test(value);
+  }
+
+  public static isMinimumLength(value: string) {
+    return value.length > 5;
+  }
+
+  public static create(props: PasswordProps): PasswordType {
+    const valueString =
+      typeof props.value === 'string' ? props.value : props.value.value;
+
+    const propsResult = Guard.againstNullOrUndefined(valueString, 'password');
+    if (!propsResult.succeeded) {
+      UserError.GenericMessageError.create('The password field is required!');
+    }
+
+    if (!this.isBcryptHash(valueString)) {
+      const isMinimumLength = this.isMinimumLength(valueString);
+      if (!isMinimumLength) {
+        return UserError.GenericMessageError.create(
+          'The password field has no minimum length!',
+        );
+      }
+
+      const hashedPass = this.hashPassword(valueString);
+      return new Password({
+        value: hashedPass,
+        hashed: true,
+      });
+    }
+
+    return new Password({ value: valueString, hashed: true });
+  }
+}
