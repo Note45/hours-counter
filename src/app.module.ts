75617ecc2a20ec7f3bbb7bfef99485397ebf5bb0
Note45 <antonion.2048@gmail.com>
2022-01-13 16:31:29 -0300

    add user module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MONGO_STRING_URI } from '../environments';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(MONGO_STRING_URI), UserModule],
   controllers: [],
   providers: [],
 })
