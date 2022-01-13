f2c20a3fd18b92937a628e4ff4a2fe071c117255
Note45 <antonion.2048@gmail.com>
2022-01-13 16:29:48 -0300

    refactor base class to implement user repository

--- src/user/infra/repository/user.repo.ts
+++ src/user/infra/repository/user.repo.ts
@@ -4,6 +4,6 @@ import { IUserRepoErrors } from './user.repo.errors';
 export type UserResponse<T> = IUserRepoErrors | T;
 
 export interface IUserRepo {
-  getClientes(userId: string): Promise<UserResponse<User>>;
+  getClienteById(id: string): Promise<UserResponse<User>>;
   createCliente(user: User): Promise<UserResponse<User>>;
 }
