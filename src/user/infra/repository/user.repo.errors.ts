9772f1c45f9cb1b431abc67da83949415e11c936
Note45 <antonion.2048@gmail.com>
2022-01-13 11:27:03 -0300

    add error class to user repository

---
+++ src/user/infra/repository/user.repo.errors.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable @typescript-eslint/no-namespace */
+
+import { DomainError } from '../../../shared/core/domain/domain-errors';
+
+export type IUserRepoErrors =
+  | UserRepoErrors.InvalidUserInfoError
+  | UserRepoErrors.UserGenericError;
+
+export namespace UserRepoErrors {
+  export class InvalidUserInfoError extends DomainError {
+    public constructor(fieldName: string) {
+      super(fieldName);
+    }
+
+    public create(fieldName: string): InvalidUserInfoError {
+      const propNameOrDefaultError = fieldName || 'Some field is invalid';
+      return new InvalidUserInfoError(propNameOrDefaultError);
+    }
+  }
+
+  export class UserGenericError extends DomainError {
+    public constructor(value: string) {
+      super(value);
+    }
+
+    public static create(errorValue: string): UserGenericError {
+      return new UserGenericError(errorValue);
+    }
+  }
+}
