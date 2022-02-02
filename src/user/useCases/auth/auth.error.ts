621bf607e7d76c989ad758e9fd3b439ef0ef68b3
note45 <antonion.2048@gmail.com>
2022-02-01 21:51:41 -0300

    add error class to use case auth

---
+++ src/user/useCases/auth/auth.error.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import { DomainError } from '../../../shared/core/domain/domain-errors';
+
+export type IAuthErrors = AuthErrors.AuthCredentialsError;
+
+export namespace AuthErrors {
+  export class AuthCredentialsError extends DomainError {
+    private constructor() {
+      super(`Email or password is wrong!`);
+    }
+
+    public static create(): AuthCredentialsError {
+      return new AuthCredentialsError();
+    }
+  }
+}
