b961bb7a5ec56c7cd45496ed14f326f3ed13b0c2
amelo <amelo@acesso.com>
2023-02-23 10:09:37 -0300

    chore: create setup module category

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MONGO_STRING_URI } from '../environments';
 import { UserModule } from './modules/user/user.module';
+import { CategoryModule } from './modules/category/category.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(MONGO_STRING_URI), UserModule],
+  imports: [
+    MongooseModule.forRoot(MONGO_STRING_URI),
+    UserModule,
+    CategoryModule,
+  ],
   controllers: [],
   providers: [],
 })
